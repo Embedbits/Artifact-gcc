b0da5fd35f14f136458bbf242d2d01e38984fe92
Mr.Nobody <marek@embedbits.com>
2026-09-03 12:41:35 +0200

    Core/1.0.0

--- ArtifactConfig.cmake
+++ ArtifactConfig.cmake
@@ -1,18 +1,46 @@
-#Set GCC path
-if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
+set(GCC_CURRENT_LIST_DIR ${CMAKE_CURRENT_LIST_DIR})
+#------------------------------------------------------------------------------#
+# Returns artifact version.
+#
+# The name of function must consist of folder name (doxygen) and postfix 
+# (_GetArtifactVersion). Otherwise the buildprocess will fail.  
+#
+# ARTIFACT_VERSION [out]: Version of artifact in format X.Y.Z
+#------------------------------------------------------------------------------#
+function(doxygen_GetArtifactVersion RET_VERSION)
 
-    set(ENV{PATH} "$ENV{PATH};${CMAKE_CURRENT_LIST_DIR}/mingw64/bin/")
-    
-else()
-    
-    set(ENV{PATH} "$ENV{PATH}:${CMAKE_CURRENT_LIST_DIR}/gcc/bin/")
-    
-endif()
+    # Execute the gcc command to get its version
+    execute_process(
+        COMMAND gcc --version
+        OUTPUT_VARIABLE ARTIFACT_VERSION
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
 
-execute_process(
-    COMMAND gcc --version
-    OUTPUT_VARIABLE ARTIFACT_VERSION
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+    string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+" VERSION "${ARTIFACT_VERSION}")
+                    
+    set(${RET_VERSION} "${VERSION}" PARENT_SCOPE)
 
-message(STATUS "Mingw version found: ${ARTIFACT_VERSION}")
+endfunction()
+
+
+#------------------------------------------------------------------------------#
+# Initialize artifact for build.
+#
+# The name of function must consist of folder name (doxygen) and postfix 
+# (_ArtifactInstall). Otherwise the buildprocess will fail.  
+#
+# ARTIFACT_BIN_PATH_ARG [in]: Path to the binary part of artifact
+#------------------------------------------------------------------------------#
+function(doxygen_ArtifactInit ARTIFACT_BIN_PATH_ARG)
+
+    if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
+
+        set(ENV{PATH} "${ARTIFACT_BIN_PATH_ARG}/mingw64/bin/;$ENV{PATH}")
+        
+    else()
+        
+        set(ENV{PATH} "${ARTIFACT_BIN_PATH_ARG}/gcc/bin/:$ENV{PATH}")
+        
+    endif()
+
+endfunction()
